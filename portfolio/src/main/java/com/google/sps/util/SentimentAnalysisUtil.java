6e958bebc9f86b18de734051ec9165832cf0a146
Kevin Lim <lyskevin@gmail.com>
2020-06-23 17:38:49 +0800

    Make language service client an instance variable (wrapped inside an Optional) and move exception handling into the constructor of SentimentAnalysisUtil class

--- portfolio/src/main/java/com/google/sps/util/SentimentAnalysisUtil.java
+++ portfolio/src/main/java/com/google/sps/util/SentimentAnalysisUtil.java
@@ -4,21 +4,28 @@ import com.google.cloud.language.v1.Document;
 import com.google.cloud.language.v1.LanguageServiceClient;
 import com.google.cloud.language.v1.Sentiment;
 import java.io.IOException;
+import java.util.Optional;
 
 public class SentimentAnalysisUtil {
-  public static float getSentimentScore(String text) {
+  private Optional<LanguageServiceClient> languageService;
+
+  public SentimentAnalysisUtil() {
+    try {
+      languageService = Optional.ofNullable(LanguageServiceClient.create());
+    } catch (IOException ioe) {
+      languageService = Optional.empty();
+    }
+  }
+
+  public float getSentimentScore(String text) {
     float score = 0;
 
-    try {
+    if (languageService.isPresent()) {
       Document document =
           Document.newBuilder().setContent(text).setType(Document.Type.PLAIN_TEXT).build();
-      LanguageServiceClient languageService = LanguageServiceClient.create();
-      Sentiment sentiment = languageService.analyzeSentiment(document).getDocumentSentiment();
-      languageService.close();
+      Sentiment sentiment =
+          languageService.get().analyzeSentiment(document).getDocumentSentiment();
       score = sentiment.getScore();
-    } catch (IOException ioe) {
-      // An IOException is thrown if the language service client cannot be instantiated.
-      // In this case, the sentiment score remains as 0.
     }
 
     return score;
